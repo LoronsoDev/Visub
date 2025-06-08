4691f67cd028a3ef4bb1e535b39aff252a2c54f4
Lorenzo Herrán <lorenzohs98@gmail.com>
2025-06-08 16:25:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     py_modules=["auto_subtitle"],
     author="Miguel Piedrafita",
     install_requires=[
-        'openai-whisper',
+        'openai-whisper @ git+https://github.com/openai/whisper.git',
     ],
     description="Automatically generate and embed subtitles into your videos",
     entry_points={
